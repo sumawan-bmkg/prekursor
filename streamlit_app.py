e98972f6687adfd380e8c14b5deee8e7e4fcc2ff
sumawan <sumawan@bmkg.go.id>
2025-10-23 10:15:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("🎈Prekursor Gempa Bumi")
 
+  st.image("https://drive.google.com/file/d/14-PkaSRayveg2M7QGu5Meo2nTh5B_6XT/view?usp=sharing")
 
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
