cf6ef09476225f94cf561031073d1d72f72e523c
sumawan <sumawan@bmkg.go.id>
2025-10-23 10:10:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈Prekursor Gempa Bumi")
-st.image("/img/prekursor.png", caption="Peta Prekursor Indonesia")
+st.image("http://202.90.198.224:8484/sopg/web/index.php?r=anomali%2Fmap&AnomaliSearch%5Btgl_anomali%5D=21-Oct-2025+-+23-Oct-2025&AnomaliSearch%5BtglAnoStart%5D=21-Oct-2025&AnomaliSearch%5BtglAnoEnd%5D=23-Oct-2025&AnomaliSearch%5Bwilayah%5D=&AnomaliSearch%5Bkode_sta%5D=&AnomaliSearch%5Bis_valid%5D=1&AnomaliSearch%5Bstatus%5D=0&AnomaliSearch%5Btingkat%5D=&AnomaliSearch%5Bbasemap%5D=OpenStreetMap.HOT&AnomaliSearch%5Bstasiun%5D=1&AnomaliSearch%5Bgempa%5D=1&AnomaliSearch%5Bsegitiga%5D=1&AnomaliSearch%5Birisan%5D=1&AnomaliSearch%5Bpatahan%5D=1", caption="Peta Prekursor Indonesia")
 
 
 st.write(
